3894faa9a03a0c4cbf39511c9586d5545702897c
Lennart Flauß <lennart.flauss@student.hs-anhalt.de>
2023-06-22 18:59:06 +0200

    Intial commit

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LoRaWAN
+{
+    class Class1
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello, World");
+            Console.In.ReadLine();
+        }
+    }
+}
