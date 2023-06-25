1690c05d9ad861fdd2a68571f27a415b559f0766
Lennart Flauß <lennart.flauss@student.hs-anhalt.de>
2023-06-25 18:17:03 +0200

    added basic server functions (WIP)

---
+++ TestServer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+
+namespace LoRaWAN
+{
+    class TestServer : Server
+    {
+        // Constructor
+        public TestServer(string uri) : base(uri)
+        {
+            // Empty construcor
+        }
+
+
+        // Start
+        public static void Main()
+        {
+            // Test, if the Server gets data from a HTTP-Post-Request
+            Server server = new TestServer("http://localhost:8080/");
+            new Thread(() => PostHttpRequest()).Start();
+            new Thread(() => ShutdownServer(server)).Start();
+            server.Start();
+        }
+
+
+        // Thread methods
+        static void PostHttpRequest()
+        {
+            Thread.Sleep(3000);
+            HttpClient client = new HttpClient();
+            client.PostAsync("http://localhost:8080/", new StringContent("Hallo Welt!"));
+        }
+
+        static void ShutdownServer(Server server)
+        {
+            Thread.Sleep(6000);
+            server.Shutdown();
+        }
+
+
+        // Method
+        public override void ProcessPacket(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
