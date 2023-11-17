2c2b9002661d5ba8571a3485903e524d711472bf
Lennart Flauß <lennart.flauss@student.hs-anhalt.de>
2023-11-17 14:57:53 +0100

    Changed UDP Port, Default Value Handling and csproj file

--- LoRaWAN Backend/SemtechProtocol/Data/Txpk.cs
+++ LoRaWAN Backend/SemtechProtocol/Data/Txpk.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using System.ComponentModel;
+using Newtonsoft.Json;
 
 namespace LoRaWAN.SemtechProtocol.Data
 {
@@ -6,33 +7,41 @@ namespace LoRaWAN.SemtechProtocol.Data
     {
         [JsonProperty("imme")]
         public bool Imme;
-        [JsonProperty("tmst")]
-        public int Tmst;
-        [JsonProperty("tmms")]
-        public long Tmms;
-        [JsonProperty("freq")]
-        public float Freq;
-        [JsonProperty("rfch")]
-        public int Rfch;
-        [JsonProperty("powe")]
-        public int Powe;
-        [JsonProperty("modu")]
+        [JsonProperty("tmst",DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [DefaultValue(-1)]
+        public int Tmst = -1;
+        [JsonProperty("tmms", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [DefaultValue(-1)]
+        public long Tmms = -1;
+        [JsonProperty("freq", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [DefaultValue(-1)]
+        public float Freq = -1;
+        [JsonProperty("rfch", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [DefaultValue(-1)]
+        public int Rfch = -1;
+        [JsonProperty("powe", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [DefaultValue(-1)]
+        public int Powe = -1;
+        [JsonProperty("modu", NullValueHandling = NullValueHandling.Ignore)]
         public string Modu;
-        [JsonProperty("datr")]
+        [JsonProperty("datr", NullValueHandling = NullValueHandling.Ignore)]
         public string Datr;
-        [JsonProperty("codr")]
+        [JsonProperty("codr", NullValueHandling = NullValueHandling.Ignore)]
         public string Codr;
-        [JsonProperty("fdev")]
-        public int Fdev;
+        [JsonProperty("fdev", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [DefaultValue(-1)]
+        public int Fdev = -1;
         [JsonProperty("ipol")]
         public bool Ipol;
-        [JsonProperty("prea")]
-        public int Prea;
-        [JsonProperty("size")]
-        public int Size;
+        [JsonProperty("prea", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [DefaultValue(-1)]
+        public int Prea = -1;
+        [JsonProperty("size", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
+        [DefaultValue(-1)]
+        public int Size = -1;
         [JsonProperty("data")]
         public string Data;
-        [JsonProperty("ncrc")]
+        [JsonProperty("ncrc", DefaultValueHandling = DefaultValueHandling.IgnoreAndPopulate)]
         public bool Ncrc;
     }
 }
