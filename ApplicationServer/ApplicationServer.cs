462acd80b46c57b1b793a3c727e390b5e83b9a41
Philipp Jäckel <philipp.jaeckel.1998@gmail.com>
2023-11-22 14:20:51 +0100

    rudimentaryApplicationServer

---
+++ ApplicationServer/ApplicationServer.cs
@@ -0,0 +1,15 @@
+﻿using LoRaWAN;
+
+namespace ApplicationServer
+{
+    public class ApplicationServer : Server
+    {
+
+        public ApplicationServer() : base(Appsettings.ApplicationServerURL) { }
+
+        public override void ProcessPacket(string json)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
