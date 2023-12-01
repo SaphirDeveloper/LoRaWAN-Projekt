e32e35fdc66050c9d6d0b51a39c9a9d65568ac51
Lennart Flauß <lennart.flauss@student.hs-anhalt.de>
2023-12-01 13:10:34 +0100

    Updated Network Server Tests

--- Unit Test/NetworkServerTest.cs
+++ Unit Test/NetworkServerTest.cs
@@ -3,36 +3,38 @@ using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using System.Net.Sockets;
 using LoRaWAN.SemtechProtocol.Data;
+using LoRaWAN.HTTP;
+using LoRaWAN;
+using Microsoft.AspNetCore.Hosting.Server;
 
 namespace Unit_Test
 {
     [TestClass]
     public class NetworkServerTest
     {
+        private Thread _serverThread;
+
+        [TestInitialize]
+        public void StartNetworkServer()
+        {
+            _serverThread = new Thread(() => Backend.CreateAndStartWebHost(new string[0], new NetworkServer.NetworkServer()));
+            _serverThread.Start();
+        }
+
         [TestMethod]
         public void NetworkServerHTTPPostRequestTest()
         {
-            // Start server
-            var server = new NetworkServer.NetworkServer();
-            server.Start();
-
             // Send HTTP-POST-Request
             string networkServerURL = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build()["NetworkServerURL"];
             Stat stat = new Stat(DateTime.Now.ToString(), 46.24000f, 3.25230f, 145, 2, 2, 2, 100.0f, 2, 2);
             StringContent content = new StringContent(JsonConvert.SerializeObject(stat), Encoding.UTF8, "application/json");
             HttpClient client = new HttpClient();
             HttpResponseMessage response = client.PostAsync(networkServerURL, content).Result;
-
-            server.Shutdown();
         }
 
         [TestMethod]
         public void NetworkServerUDPTest()
         {
-            // Start server
-            var server = new NetworkServer.NetworkServer();
-            server.Start();
-
             // Send UDP message
             byte[] message = Encoding.ASCII.GetBytes("Hello Network Server!");
             UdpClient udpClient = new UdpClient(12345);
@@ -40,8 +42,6 @@ namespace Unit_Test
             udpClient.Send(message);
             udpClient.Close();
             udpClient.Dispose();
-
-            server.Shutdown();
         }
     }
 }
